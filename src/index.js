afcb84e58c67e960a44b157567ecaea3284301b3
KliffUndersn <83068780+KliffUndersn@users.noreply.github.com>
2021-09-12 17:05:24 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import fetchCountries from './fetchCountries'
 import { debounce } from "debounce";
+import Notiflix from "notiflix";
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
@@ -12,25 +13,30 @@ const searchInput = (e) => {
     const querySearch = e.target.value.trim();
 
 fetchCountries(querySearch)
-    .then(data => data.length >= 11? refs.list.innerHTML = `Too many matches found. Please enter a more specific name.`: data)
-    .then(data => data.length === 1?refs.list.innerHTML = data.map(
-        ({name, capital,population,flag,languages})=>
-        `<li style="list-style: none;display: block;flex-direction: column;align-items: center;">
-        <div style="flex-direction: start;"><img style="margin-right: 30px;" src="${flag}"width=80px height=40px alt=""> 
-        <h1 style="display:inline"> ${name}</h1></div>
-        <p><span style="font-weight: bold;">Capital:</span>  ${capital}</p>
-        <p><span style="font-weight: bold;">Population:</span>   ${population}</p>
-        <p><span style="font-weight: bold;">Lenguages:</span>   ${languages.map(leng=>leng.nativeName)}</p>
-        
-        </li>`).join(''):
-    refs.list.innerHTML = data.map(
-        ({name, capital,population,flag,languages})=>
-        `<li style="list-style: none"><img src="${flag}"width=20px alt=""> ${name}
-        </li>`).join(''))
+    .then(data => {
+if (data.length >= 11){Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`)}
+else if (data.length === 1){refs.list.innerHTML = data.map(
+    ({name, capital,population,flag,languages})=>
+    `<li style="list-style: none;display: block;flex-direction: column;align-items: center;">
+    <div style="flex-direction: start;"><img style="margin-right: 30px;" src="${flag}"width=80px height=40px alt=""> 
+    <h1 style="display:inline"> ${name}</h1></div>
+    <p><span style="font-weight: bold;">Capital:</span>  ${capital}</p>
+    <p><span style="font-weight: bold;">Population:</span>   ${population}</p>
+    <p><span style="font-weight: bold;">Lenguages:</span>   ${languages.map(leng=>leng.nativeName)}</p>
+    
+    </li>`).join('')}
+else 
+{ refs.list.innerHTML = data.map(
+    ({name, capital,population,flag,languages})=>
+    `<li style="list-style: none"><img src="${flag}"width=20px alt=""> ${name}
+    </li>`).join('')}})
+
     .catch((e) => {
-        console.log(e);
-        // refs.list.innerHTML =`"Oops, there is no country with that name"`
+        // console.log(e);
+        Notiflix.Notify.failure('Oops, there is no country with that name');
     });
+
+    
 }
  
 refs.form.addEventListener('input', debounce(searchInput,DEBOUNCE_DELAY))
